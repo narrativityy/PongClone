ae3fab2e6958ea9069bb650db221feb093819b62
narrativityy <jake_crowley@comcast.net>
2025-01-06 17:06:10 -0800

    added single player mode with a simple ai

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# Example file showing a circle moving on screen
 import pygame
 
 # pygame setup
@@ -30,18 +29,29 @@ ball_speed = 300
 ball_direction_x = 1
 ball_direction_y = 1
 
+single_player = False
+single_player_button_color = "red"
+
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_SPACE:
+                single_player = not single_player
+                if single_player:
+                    single_player_button_color = "green"
+                else:
+                    single_player_button_color = "red"
+                print(single_player)
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.fill("black")
 
-    player1_score_text = my_font.render("Player 1: " + str(player1_score), False, "white")
-    player2_score_text = my_font.render("Player 2: " + str(player2_score), False, "white")
+    player1_score_text = my_font.render("Player 1: " + str(player1_score), True, "white")
+    player2_score_text = my_font.render("Player 2: " + str(player2_score), True, "white")
     screen.blit(player1_score_text, (10, 10))
     screen.blit(player2_score_text, (screen.get_width() - player2_score_text.get_width() - 10, 10))
 
@@ -49,6 +59,12 @@ while running:
     pygame.draw.rect(screen, 'red', (paddle_b_left, paddle_b_top, paddle_width, paddle_height), 0, 10)
 
     pygame.draw.circle(screen, "white", (ball_pos_x, ball_pos_y), 25)
+
+    button_text = my_font.render("Single Player", True, single_player_button_color)
+    screen.blit(button_text, (screen.get_width() - 10 - button_text.get_width(), screen.get_height() - 10 - button_text.get_height()))
+
+    mouse_pos = pygame.mouse.get_pos()
+    mouse_pressed = pygame.mouse.get_pressed()
 
     if ball_pos_y - 25 <= 0:
         ball_direction_y = 1
@@ -85,16 +101,28 @@ while running:
             paddle_a_top = screen.get_height() - paddle_height
         else:
             paddle_a_top += 300 * dt
-    if keys[pygame.K_UP]:
-        if paddle_b_top <= 0:
-            paddle_b_top = 0
-        else:
-            paddle_b_top -= 300 * dt
-    if keys[pygame.K_DOWN]:
-        if paddle_b_top >= screen.get_height() - paddle_height:
-            paddle_b_top = screen.get_height() - paddle_height
-        else:
-            paddle_b_top += 300 * dt
+    if not single_player:
+        if keys[pygame.K_UP]:
+            if paddle_b_top <= 0:
+                paddle_b_top = 0
+            else:
+                paddle_b_top -= 300 * dt
+        if keys[pygame.K_DOWN]:
+            if paddle_b_top >= screen.get_height() - paddle_height:
+                paddle_b_top = screen.get_height() - paddle_height
+            else:
+                paddle_b_top += 300 * dt
+    else:
+        if ball_pos_y < paddle_b_top + paddle_height / 2:
+            if paddle_b_top <= 0:
+                paddle_b_top = 0
+            else:
+                paddle_b_top -= 200 * dt
+        if ball_pos_y > paddle_b_top + paddle_height / 2:
+            if paddle_b_top >= screen.get_height() - paddle_height:
+                paddle_b_top = screen.get_height() - paddle_height
+            else:
+                paddle_b_top += 200 * dt
 
     # flip() the display to put your work on screen
     pygame.display.flip()
