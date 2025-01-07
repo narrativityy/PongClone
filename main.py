23f23b4bb784ed90f7d13541a76d04b2d663a699
narrativityy <jake_crowley@comcast.net>
2025-01-06 16:33:02 -0800

    added paddles and movement

--- main.py
+++ main.py
@@ -8,7 +8,15 @@ clock = pygame.time.Clock()
 running = True
 dt = 0
 
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+paddle_height = 200
+paddle_width = 30
+
+paddle_a_top = screen.get_height() / 2 - (paddle_height / 2)
+paddle_a_left = 10
+
+paddle_b_top = screen.get_height() / 2 - (paddle_height / 2)
+paddle_b_left = screen.get_width() - paddle_width - 10
+
 
 while running:
     # poll for events
@@ -20,17 +28,18 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
 
-    pygame.draw.circle(screen, "red", player_pos, 40)
+    pygame.draw.rect(screen, "green", (paddle_a_left, paddle_a_top, paddle_width, paddle_height), 0, 10)
+    pygame.draw.rect(screen, 'red', (paddle_b_left, paddle_b_top, paddle_width, paddle_height), 0, 10)
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
+        paddle_a_top -= 300 * dt
     if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+        paddle_a_top += 300 * dt
+    if keys[pygame.K_UP]:
+        paddle_b_top -= 300 * dt
+    if keys[pygame.K_DOWN]:
+        paddle_b_top += 300 * dt
 
     # flip() the display to put your work on screen
     pygame.display.flip()
