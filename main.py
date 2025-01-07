acd64c3684625f41605e0fce0baea9f704ed73e3
narrativityy <jake_crowley@comcast.net>
2025-01-06 17:17:16 -0800

    added pause functionality

--- main.py
+++ main.py
@@ -32,6 +32,8 @@ ball_direction_y = 1
 single_player = False
 single_player_button_color = "red"
 
+paused = False
+
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -39,13 +41,15 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_SPACE:
+            if event.key == pygame.K_1 and not paused:
                 single_player = not single_player
                 if single_player:
                     single_player_button_color = "green"
                 else:
                     single_player_button_color = "red"
                 print(single_player)
+            if event.key == pygame.K_SPACE:
+                paused = not paused
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("black")
@@ -66,64 +70,68 @@ while running:
     mouse_pos = pygame.mouse.get_pos()
     mouse_pressed = pygame.mouse.get_pressed()
 
-    if ball_pos_y - 25 <= 0:
-        ball_direction_y = 1
-    if ball_pos_y + 25 >= screen.get_height():
-        ball_direction_y = -1
-    if ball_pos_x <= 10 + paddle_width + 25 and paddle_a_top <= ball_pos_y <= paddle_a_top + paddle_height:
-        ball_direction_x = 1
-    if ball_pos_x >= screen.get_width() - 10 - paddle_width - 25 and paddle_b_top <= ball_pos_y <= paddle_b_top + paddle_height:
-        ball_direction_x = -1
-    if ball_pos_x - 25 <= 0:
-        player2_score += 1
-        print(player2_score)
-        ball_pos_x = screen.get_width() / 2
-        ball_pos_y = screen.get_height() / 2
-        ball_direction_x = 1
-    if ball_pos_x + 25 >= screen.get_width():
-        player1_score += 1
-        print(player1_score)
-        ball_pos_x = screen.get_width() / 2
-        ball_pos_y = screen.get_height() / 2
-        ball_direction_x = -1
+    if not paused:
+        if ball_pos_y - 25 <= 0:
+            ball_direction_y = 1
+        if ball_pos_y + 25 >= screen.get_height():
+            ball_direction_y = -1
+        if ball_pos_x <= 10 + paddle_width + 25 and paddle_a_top <= ball_pos_y <= paddle_a_top + paddle_height:
+            ball_direction_x = 1
+        if ball_pos_x >= screen.get_width() - 10 - paddle_width - 25 and paddle_b_top <= ball_pos_y <= paddle_b_top + paddle_height:
+            ball_direction_x = -1
+        if ball_pos_x - 25 <= 0:
+            player2_score += 1
+            print(player2_score)
+            ball_pos_x = screen.get_width() / 2
+            ball_pos_y = screen.get_height() / 2
+            ball_direction_x = 1
+        if ball_pos_x + 25 >= screen.get_width():
+            player1_score += 1
+            print(player1_score)
+            ball_pos_x = screen.get_width() / 2
+            ball_pos_y = screen.get_height() / 2
+            ball_direction_x = -1
 
-    ball_pos_x += ball_speed * dt * ball_direction_x
-    ball_pos_y += ball_speed * dt * ball_direction_y
+        ball_pos_x += ball_speed * dt * ball_direction_x
+        ball_pos_y += ball_speed * dt * ball_direction_y
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        if paddle_a_top <= 0:
-            paddle_a_top = 0
-        else:
-            paddle_a_top -= 300 * dt
-    if keys[pygame.K_s]:
-        if paddle_a_top >= screen.get_height() - paddle_height:
-            paddle_a_top = screen.get_height() - paddle_height
-        else:
-            paddle_a_top += 300 * dt
-    if not single_player:
-        if keys[pygame.K_UP]:
-            if paddle_b_top <= 0:
-                paddle_b_top = 0
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            if paddle_a_top <= 0:
+                paddle_a_top = 0
             else:
-                paddle_b_top -= 300 * dt
-        if keys[pygame.K_DOWN]:
-            if paddle_b_top >= screen.get_height() - paddle_height:
-                paddle_b_top = screen.get_height() - paddle_height
+                paddle_a_top -= 300 * dt
+        if keys[pygame.K_s]:
+            if paddle_a_top >= screen.get_height() - paddle_height:
+                paddle_a_top = screen.get_height() - paddle_height
             else:
-                paddle_b_top += 300 * dt
+                paddle_a_top += 300 * dt
+        if not single_player:
+            if keys[pygame.K_UP]:
+                if paddle_b_top <= 0:
+                    paddle_b_top = 0
+                else:
+                    paddle_b_top -= 300 * dt
+            if keys[pygame.K_DOWN]:
+                if paddle_b_top >= screen.get_height() - paddle_height:
+                    paddle_b_top = screen.get_height() - paddle_height
+                else:
+                    paddle_b_top += 300 * dt
+        else:
+            if ball_pos_y < paddle_b_top + paddle_height / 2:
+                if paddle_b_top <= 0:
+                    paddle_b_top = 0
+                else:
+                    paddle_b_top -= 200 * dt
+            if ball_pos_y > paddle_b_top + paddle_height / 2:
+                if paddle_b_top >= screen.get_height() - paddle_height:
+                    paddle_b_top = screen.get_height() - paddle_height
+                else:
+                    paddle_b_top += 200 * dt
     else:
-        if ball_pos_y < paddle_b_top + paddle_height / 2:
-            if paddle_b_top <= 0:
-                paddle_b_top = 0
-            else:
-                paddle_b_top -= 200 * dt
-        if ball_pos_y > paddle_b_top + paddle_height / 2:
-            if paddle_b_top >= screen.get_height() - paddle_height:
-                paddle_b_top = screen.get_height() - paddle_height
-            else:
-                paddle_b_top += 200 * dt
-
+        pygame.draw.rect(screen, "white", (screen.get_width() / 2 - 30, screen.get_height() / 2 - 100, 50, 200), 0, 10)
+        pygame.draw.rect(screen, "white", (screen.get_width() / 2 + 30, screen.get_height() / 2 - 100, 50, 200), 0, 10)
+        
     # flip() the display to put your work on screen
     pygame.display.flip()
 
